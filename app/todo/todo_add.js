4c11f937f91ede9b37454f0182dc956028fc42d4
shiliangl <shiliangwei@cloudolp.com>
2017-11-27 08:42:54 +0800

    learn how to use state from redux

--- app/todo/todo_add.js
+++ app/todo/todo_add.js
@@ -3,20 +3,26 @@ import { AddToDo } from "../redux/action/index";
 
 import { connect } from "react-redux";
 import todos from "../redux/reducers/index";
- class Todo_Add extends Component {
+class Todo_Add extends Component {
   handleClick(e) {
-    console.log(this.props);
-    const { dispatch } = this.props
+    const { dispatch } = this.props;
     e.preventDefault();
-    if (this.input.value.trim() === '') return
+    if (this.input.value.trim() === "") return;
     dispatch(AddToDo(this.input.value));
     this.input.value = "";
+  }
+
+  onKeyup(e) {
+    if (e.keyCode === 13) {
+      console.log('回车')
+    }
   }
 
   render() {
     return (
       <div>
         <input
+          onKeyUp={this.onKeyup}
           type="text"
           ref={node => {
             this.input = node;
--- app/todo/todo_list.js
+++ app/todo/todo_list.js
@@ -1,14 +1,30 @@
 import React, { Component } from "react";
-class TodoList extends Component {
+import { connect } from "react-redux";
 
- 
-  render() {
-    return (
-      <ul className="dodo-list">
-        <div>显示内容测试</div>
-      </ul>
-    );
+const TodoList = ({ todoList }) => {
+  return (
+    <ul className="dodo-list">
+      {todoList.map((item, index) => {
+        return (
+          <p key={item.id} onClick={() => onTodoList()}>
+            {item.text}
+          </p>
+        );
+      })}
+    </ul>
+  );
+};
+
+//将 state 中的值映射到 prps 中去
+const mapStateToProps = state => {
+  return { todoList: state.todos };
+};
+
+// 猜测 将方法 触发到组件中
+const mapDispatchToProps = dispatch => {
+  return {
+    
   }
-}
+};
 
-export default TodoList;
+export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
