cfb6d3d7a53790df8bc74374ea9579bb5cd285c6
shiliangl <shiliangwei@cloudolp.com>
2017-11-29 09:20:16 +0800

    style use stick footer

--- app/page/App.jsx
+++ app/page/App.jsx
@@ -1,21 +1,19 @@
 //页面输出位置
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
 import NavList from "../components/NavList/NavList";
 export default class App extends Component {
   render() {
-    return <div id="wrapApp">
-        <div className="content">
-          <div className="main">
-            <div> {this.props.children} </div>
-          </div>
+    return (
+      <div className="wrapApp">
+        <div className="main">
+          <div> {this.props.children} </div>
         </div>
         <div className="footer">
           <NavList />
         </div>
-      </div>;
+      </div>
+    );
   }
- 
 }
-
