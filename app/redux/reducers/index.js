061ed29bbb440c6d489e9e9f3645dfad82ba993e
shiliangl <shiliangwei@cloudolp.com>
2018-03-15 08:42:37 +0800

    make_project

--- app/redux/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-import todos from "./todo";
-import visibilityFilter from "./visibilityFilter";
-
-const todoApp = combineReducers({
-  todos,
-  visibilityFilter
-});
-export default todoApp;
--- app/redux/store/configStore.js
+++
@@ -1,7 +0,0 @@
-import { createStore } from 'redux'
-import rootReducer from '../reducers'
-
-export default function configStore(initialState) {
-    const store = createStore(rootReducer, initialState,window.devToolsExtension ? window.devToolsExtension() : undefined)
-    return store
-}
