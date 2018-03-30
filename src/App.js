f8435176a9b4018e6e96d65d4f04eca699bba973
shiliangl <shiliangwei@cloudolp.com>
2018-03-30 13:22:03 +0800

    install redux

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { Button } from 'antd-mobile';
+
+import { createStore } from 'redux'
 
 class App extends Component {
   render() {
@@ -6,6 +9,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Welcome to React</h1>
+          <Button type="primary" size="small" inline>Start</Button>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
